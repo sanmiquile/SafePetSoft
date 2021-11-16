af8267941e5746e6f96fbbfc018fd715a30ccfbe
Sandra Milena Quintero Leal <smquinterol@uqvirtual.edu.co>
2021-11-15 19:35:40 -0500

    Se enlazan las interfaces, y se realizan mejoras visuales

--- src/main/java/co/edu/uniquindio/software/safepet/SafepetRestApplication.java
+++ src/main/java/co/edu/uniquindio/software/safepet/SafepetRestApplication.java
@@ -1,12 +1,12 @@
 package co.edu.uniquindio.software.safepet;
 
+import javax.annotation.security.DeclareRoles;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/**
- *
- */
+
+@DeclareRoles({"EmpleadoSafePet","CentroVeterinario","Afiliado"})
 @ApplicationPath("/data")
 @ApplicationScoped
 public class SafepetRestApplication extends Application {
